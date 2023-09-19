e23632842fa31b90ed5a828c62d1174fb68c097d
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-09-19 21:07:51 +0600

    added firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,9 +10,9 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  appId: import.meta.env.VITE_APPID,
 };
-console.log(import.meta.env.VITE_APIKEY)
+console.log(import.meta.env.VITE_APIKEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
