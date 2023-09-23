58aa6223b00ad96c8e09abcc52333e17b5a5f47b
Bayshakhe <bayshakhepramanik@gmail.com>
2023-09-23 15:28:30 +0600

    merge some conflict

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,19 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/LoginPage/Login";
 import Register from "../pages/RegisterPage/Register";
+import DashboardLayout from "../pages/DashboardPage/DashboardLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/allAlumni",
+        element: <div>All Alumni</div>,
+      },
+    ],
   },
   {
     path: "/login",
-    element: <Login/>,
+    element: <Login />,
   },
   {
     path: "/register",
-    element: <Register/>,
+    element: <Register />,
   },
 ]);
 
